b7ded3b9c17acbef5bcf1f5f100cf9b89cb8638c
Larissa Dinix <lariediniz@gmail.com>
2020-03-30 15:46:34 -0300

    Minha primeira alteração

---
+++ Gift101-Xcode-2020/Gift101-Xcode-2020/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Gift101-Xcode-2020
+//
+//  Created by Larissa Diniz on 30/03/20.
+//  Copyright © 2020 Larissa Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        print("O projeto já começou")
+    }
+
+
+}
+
---
+++ lariediniz/lariediniz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lariediniz
+//
+//  Created by Larissa Diniz on 30/03/20.
+//  Copyright © 2020 Larissa Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
